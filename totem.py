d75da2b77f03e953113aaa38abeb5ada84af7b07
totem <victorkich@yahoo.com.br>
2021-07-22 23:44:12 -0300

    Implementando pyqt

--- totem.py
+++ totem.py
@@ -12,7 +12,15 @@ import numpy as np
 from torch.autograd import Variable
 from datetime import datetime
 
+import sys
+from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+
 import cv2
+
+cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN-5, cv2.WINDOW_FULLSCREEN)
 
 
 def rotate_bound(image, angle):
@@ -41,46 +49,122 @@ stop = False
 
 
 def update_screen():
-    global t_frame, x1, x2, y1, y2, cls_pred, conf, stop
-    while stop:
+    global x1, x2, y1, y2, conf, cls_pred
+    while not stop:
         # Black image
-        frame = np.zeros((x, y, 3), np.uint8)
+        f = np.zeros((x, y, 3), np.uint8)
 
-        frame[start_new_i_height: (start_new_i_height + v_height),
+        f[start_new_i_height: (start_new_i_height + v_height),
         start_new_i_width: (start_new_i_width + v_width)] = t_frame
 
         # resizing to [416x 416]
-        frame = cv2.UMat(frame)
-        frame = cv2.resize(frame, (opt.frame_size, opt.frame_size))
+        f = cv2.UMat(f)
+        f = cv2.resize(f, (opt.frame_size, opt.frame_size))
         # [BGR -> RGB]
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.UMat.get(frame)
+        f = cv2.cvtColor(f, cv2.COLOR_BGR2RGB)
+        f = cv2.UMat.get(f)
         # [[0...255] -> [0...1]]
-        frame = np.asarray(frame) / 255
+        f = np.asarray(f) / 255
         # [[3, 416, 416] -> [416, 416, 3]]
-        frame = np.transpose(frame, [2, 0, 1])
+        f = np.transpose(f, [2, 0, 1])
         # [[416, 416, 3] => [416, 416, 3, 1]]
-        frame = np.expand_dims(frame, axis=0)
+        f = np.expand_dims(f, axis=0)
         # [np_array -> tensor]
-        frame = torch.Tensor(frame)
+        f = torch.Tensor(f)
 
         # [tensor -> variable]
-        frame = Variable(frame.type(Tensor))
+        f = Variable(f.type(Tensor))
 
         # Get detections
         with torch.no_grad():
-            detections = model(frame)
+            detections = model(f)
         detections = non_max_suppression_output(detections, opt.conf_thres, opt.nms_thres)
 
         # For each detection in detections
         detection = detections[0]
         if detection is not None:
-            for x1, y1, x2, y2, conf, cls_conf, cls_pred in detection:
+            for xx1, yy1, xx2, yy2, conf, cls_conf, cls_pred in detection:
                 # Accommodate bounding box in original frame
-                x1 = int(x1 * mul_constant - start_new_i_width)
-                y1 = int(y1 * mul_constant - start_new_i_height)
-                x2 = int(x2 * mul_constant - start_new_i_width)
-                y2 = int(y2 * mul_constant - start_new_i_height)
+                xx1 = int(xx1 * mul_constant - start_new_i_width)
+                yy1 = int(yy1 * mul_constant - start_new_i_height)
+                xx2 = int(xx2 * mul_constant - start_new_i_width)
+                yy2 = int(yy2 * mul_constant - start_new_i_height)
+            #_, _, _, _, conf, cls_conf, cls_pred = detection
+            x1 = xx1
+            y1 = yy1
+            x2 = xx2
+            y2 = yy2
+        else:
+            x1 = x2 = y1 = y2 = conf = cls_pred = 0
+        time.sleep(0.1)
+
+
+class Thread(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def run(self):
+        global fps, frames
+        while True:
+            # frame extraction
+            _, org_frame = cap.read()
+            org_frame = rotate_bound(org_frame, 90)
+            t_frame = cv2.UMat.get(org_frame)
+
+            # Bounding box making and setting Bounding box title
+            if int(cls_pred) == 0:
+                # WITH_MASK
+                cv2.rectangle(org_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            else:
+                # WITHOUT_MASK
+                cv2.rectangle(org_frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.putText(org_frame, classes[int(cls_pred)] + ": %.2f" % conf, (x1, y1 + t_size[1]), cv2.FONT_HERSHEY_PLAIN, 1, [225, 255, 255], 2)
+
+            # CURRENT TIME SHOWING
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+
+            # FPS PRINTING
+            cv2.rectangle(org_frame, (0, 0), (175, 20), (0, 0, 0), -1)
+            cv2.putText(org_frame, current_time + " FPS : %3.2f" % fps, (0, t_size[1] + 2),
+                        cv2.FONT_HERSHEY_PLAIN, 1, [255, 255, 255], 1)
+            org_frame = cv2.UMat.get(org_frame)
+
+            frames += 1
+            fps = frames / (time.time() - start)            
+
+            h, w, ch = org_frame.shape
+            bytesPerLine = ch * w
+            convertToQtFormat = QImage(org_frame.data, w, h, bytesPerLine, QImage.Format_RGB888)
+            p = convertToQtFormat.scaled(480, 640, Qt.KeepAspectRatio)
+            self.changePixmap.emit(p)
+
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 Video'
+        self.left = 100
+        self.top = 100
+        self.width = 480
+        self.height = 640
+        self.initUI()
+
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(1800, 1200)
+        # create a label
+        self.label = QLabel(self)
+        self.label.move(280, 120)
+        self.label.resize(640, 480)
+        th = Thread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+        self.show()
 
 
 if __name__ == "__main__":
@@ -131,7 +215,7 @@ if __name__ == "__main__":
     _, frame = cap.read()
     frame = rotate_bound(frame, 90)
     t_frame = cv2.UMat.get(frame)
-    v_height, v_width = frame.shape[:2]
+    v_height, v_width = t_frame.shape[:2]
 
     # For a black image
     x = y = v_height if v_height > v_width else v_width
@@ -149,51 +233,14 @@ if __name__ == "__main__":
     frames = fps = 0
     start = time.time()
     x1 = x2 = y1 = y2 = conf = cls_pred = 0
-    x = threading.Thread(target=update_screen, args=())
-    x.start()
+    th = threading.Thread(target = update_screen)
+    th.setDaemon(True)
+    th.start()
 
-    while True:
-        # frame extraction => resizing => [BGR -> RGB] => [[0...255] -> [0...1]] => [[3, 416, 416] -> [416, 416, 3]]
-        #                       => [[416, 416, 3] => [416, 416, 3, 1]] => [np_array -> tensor] => [tensor -> variable]
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_()
 
-        # frame extraction
-        _, org_frame = cap.read()
-        org_frame = rotate_bound(org_frame, 90)
-        t_frame = cv2.UMat.get(frame)
+    #cap.release()
+    #cv2.destroyAllWindows()
 
-        # Bounding box making and setting Bounding box title
-        if int(cls_pred) == 0:
-            # WITH_MASK
-            org_frame = cv2.rectangle(t_frame, (x1, y1), (x2, y2), (0, 255, 0), 2).copy()
-        else:
-            # WITHOUT_MASK
-            org_frame = cv2.rectangle(t_frame, (x1, y1), (x2, y2), (0, 0, 255), 2).copy()
-        cv2.putText(org_frame, classes[int(cls_pred)] + ": %.2f" % conf, (x1, y1 + t_size[1]), cv2.FONT_HERSHEY_PLAIN,
-                    1, [225, 255, 255], 2)
-        org_frame = cv2.UMat.get(t_frame)
-
-        # CURRENT TIME SHOWING
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-
-        # FPS PRINTING
-        org_frame = cv2.UMat(org_frame)
-        cv2.rectangle(org_frame, (0, 0), (175, 20), (0, 0, 0), -1)
-        cv2.putText(org_frame, current_time + " FPS : %3.2f" % fps, (0, t_size[1] + 2),
-                    cv2.FONT_HERSHEY_PLAIN, 1,
-                    [255, 255, 255], 1)
-        org_frame = cv2.UMat.get(org_frame)
-
-        frames += 1
-        fps = frames / (time.time() - start)
-
-        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-        cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN-5, cv2.WINDOW_FULLSCREEN)
-
-        cv2.imshow('frame', org_frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            stop = True
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
