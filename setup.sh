3a6607fc20ca57c8fc5a4360dac86470d590df06
victorkich <victorkich@yahoo.com.br>
2021-08-12 19:09:26 -0300

    Update totem.py and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt install npm
 sudo npm install pm2@latest -g
 pm2 startup
-sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u totem --hp /home/totem
+sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u totem --hp /home/git/totem
 pm2 start totem.sh
 pm2 save
--- totem.py
+++ totem.py
@@ -11,6 +11,7 @@ import os
 
 cv2.namedWindow('TotemUFSM', cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty('TotemUFSM', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+BIAS = 0.2  # How much bigger the bias is, more sensible is the 'with mask' prediction (put a number between 0 and 1)
 
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
@@ -24,9 +25,9 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
     # initialize our list of faces, their corresponding locations,
     # and the list of predictions from our face mask network
-    faces = []
-    locs = []
-    preds = []
+    faces = list()
+    locs = list()
+    preds = list()
 
     # loop over the detections
     for i in range(0, detections.shape[2]):
@@ -66,7 +67,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
         preds = maskNet.predict(faces, batch_size=32)
 
     # return a 2-tuple of the face locations and their corresponding locations
-    return (locs, preds)
+    return locs, preds
 
 
 def rotate_bound(image, angle):
@@ -94,7 +95,6 @@ def rotate_bound(image, angle):
     return cv2.warpAffine(image, M, (nW, nH))
 
 
-stop = False
 def compute_bound():
     global locs, preds
     try:
@@ -104,6 +104,19 @@ def compute_bound():
                 (locs, preds) = detect_and_predict_mask(compute_frame, faceNet, maskNet)
     except:
         os.system("reboot")
+
+
+def gate_control():
+    global gate
+    try:
+        while not stop:
+            data = arduino.readline()
+            if data == 0:
+                gate = True
+            time.sleep(0.1)
+    except:
+        # os.system("reboot")
+        pass
 
 
 def draw_frame(image, c):
@@ -121,8 +134,12 @@ def draw_frame(image, c):
     return image
 
 
+# stop running all processes
+stop = False
+gate = True  # False seams it is in opening process, True for already closed gate
+
 # start arduino connection
-#arduino = serial.Serial(port='COM0', baudrate=115200, timeout=0.1)
+# arduino = serial.Serial(port='USB0', baudrate=115200, timeout=0.1)
 
 # load our serialized face detector model from disk
 print("[INFO] loading face detector model...")
@@ -135,10 +152,12 @@ print("[INFO] loading face mask detector model...")
 maskNet = load_model("mask.model")
 
 # camara capture
-cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)10/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv2.CAP_GSTREAMER)
+# cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)10/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(0)
 assert cap.isOpened(), 'Cannot capture source'
 
 ret, frame = cap.read()
+frame_shape = frame.shape
 frame = rotate_bound(frame, 270)
 compute_frame = cv2.UMat.get(frame)
 
@@ -150,30 +169,40 @@ preds = list()
 c_fps = list()
 static_frame_time = time.time()
 static_frame = False
+post_delay_time = time.time()
+post_delay = False
 data = 0
 
 cb = threading.Thread(target=compute_bound)
 cb.setDaemon(True)
 cb.start()
 
+#gate_thread = threading.Thread(target=gate_control)
+#gate_thread.setDaemon(True)
+#gate_thread.start()
+
 start = time.time()
 while True:
     try:
-        if not time.time() - static_frame_time < 3 and not static_frame:
+        if static_frame:
+            if time.time() - static_frame_time >= 3:
+                static_frame = False
+                post_delay_time = time.time()
+                post_delay = True
+        else:
             ret, frame = cap.read()
             if not ret:
                 pass
 
-            #data = arduino.readline()
             color = (0, 0, 0)
             frame = rotate_bound(frame, 270)
             compute_frame = cv2.UMat.get(frame)
 
             # loop over the detected face locations and their corresponding locations
             if locs:
-                big_w = big_h = 0
-                for (box, pred) in zip(locs, preds):
+                big_w = big_h = mask = withoutMask = startX = startY = 0
 
+                for (box, pred) in zip(locs, preds):
                     # unpack the bounding box and predictions
                     (s_x, s_y, e_x, e_y) = box
                     if big_h < e_y - s_y and big_w < e_x - s_x:
@@ -186,24 +215,32 @@ while True:
                 startX, startY = max(startX, 0), max(startY, 0)
 
                 # determine the class label and color we'll use to draw the bounding box and text
-                label = "Com Mascara" if mask > withoutMask else "Sem Mascara"
+                label = "Com Mascara" if mask + (mask*BIAS) > withoutMask - (withoutMask*BIAS) else "Sem Mascara"
                 color = (0, 255, 0) if label == "Com Mascara" else (0, 0, 255)
+
+                if big_h >= frame_shape[1] * 0.25 and big_w >= frame_shape[0] * 0.25:
+                    if not post_delay:
+                        # opening the gate
+                        if label == "Com Mascara" and gate:
+                            # arduino.write(bytes(0, 'utf-8'))
+                            static_frame = True
+                            static_frame_time = time.time()
+                        elif label == "Sem Mascara" and gate:
+                            static_frame = True
+                            static_frame_time = time.time()
+                            cv2.putText(frame, "Por favor, utilize mascara!", (round(frame_shape[0]/25), round(frame_shape[1]/1.3)),
+                                        cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 3)
+                    else:
+                        if time.time() - post_delay_time >= 3:
+                            post_delay = False
 
                 # include the probability in the label
                 label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
                 # display the label and bounding box rectangle on the output frame
-                cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2), cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color,
+                            2), cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-                # opening the gate 
-                if label == "Com Mascara" and data == 0: # data == 0 means that the gate can be opened
-                    arduino.write(bytes(1, 'utf-8'))
-                    static_frame = True
-                    static_frame_time = time.time()
-                elif label == "Sem Mascara":
-                    static_frame = True
-                    static_frame_time = time.time()
-        
             # draw the bounds of the image
             frame = cv2.UMat.get(frame)
             frame = draw_frame(frame, color)
@@ -219,7 +256,8 @@ while True:
             if len(c_fps) > 60:
                 c_fps.pop(0)
             fps = sum(c_fps) / len(c_fps)
-            cv2.putText(frame, current_time + " FPS : %3.2f" % fps, (0, t_size[1] + 2), cv2.FONT_HERSHEY_PLAIN, 1, [255, 255, 255], 1)
+            cv2.putText(frame, current_time + " FPS : %3.2f" % fps, (0, t_size[1] + 2), cv2.FONT_HERSHEY_PLAIN, 1,
+                        [255, 255, 255], 1)
 
         # show the output frame
         cv2.imshow("TotemUFSM", frame)
@@ -227,7 +265,7 @@ while True:
             stop = True
             break
     except:
-        #os.system("reboot")
+        # os.system("reboot")
         pass
 
 cap.release()
